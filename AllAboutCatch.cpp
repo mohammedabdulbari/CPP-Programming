83ec959fe4c285fb43422f6ef6ac8d59fb8c6918
mohammedabdulbari <mohammed.bari@gmail.com>
2021-02-23 18:14:19 +0530

    Update AllAboutCatch.cpp

--- AllAboutCatch.cpp
+++ AllAboutCatch.cpp
@@ -2,36 +2,31 @@
 using namespace std;
     
 class MyException1: exception
-    {
-        
-    };
+{        
+};
     
 class MyException2:public MyException1
-    {
-        
-    };
+{     
+};
     
 int main()
-    {
-	    try
-	        {
-		        throw MyException1();
-	        }
-	        
-	    catch(MyException2 e)
-	        {
-		        cout<<"int catch"<<endl;
-        	}
-        	
-	    catch(MyException1 e)
-	        {
-	        	cout<<"double catch"<<endl;
-	        }
-	        
-	    catch(...)
-	        {
-		        cout<<"all catch"<<endl;
-	        }
+{
+	try
+	{
+		throw MyException1();
+	}
+	catch(MyException2 e)
+	{
+		cout<<"int catch"<<endl;
+	}	
+	catch(MyException1 e)
+	{
+		cout<<"double catch"<<endl;
+	}        
+	catch(...)
+	{
+		cout<<"all catch"<<endl;
+	}
 	        
     }
     
